3eeba5406cc207517321e38d15fd776828f997b0
trcmt <freefloww@outlook.com>
2024-06-05 01:12:16 +0400

    create lesson1:task5

---
+++ lesson1/task5/Program.cs
@@ -0,0 +1,9 @@
+﻿int N = 100 ;
+int current_number = 1;
+while (current_number<=N)
+{
+    //Console.WriteLine(current_number);
+    Console.Write(current_number);
+    Console.Write(' ');
+    current_number = current_number + 1;
+}
