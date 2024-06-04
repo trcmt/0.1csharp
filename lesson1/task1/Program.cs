f5293a550789f54f0cb7c42493f53062c186d8d8
trcmt <freefloww@outlook.com>
2024-06-04 22:11:53 +0400

    add new variable

--- lesson1/task1/Program.cs
+++ lesson1/task1/Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Hello, World!");
+int value = 100;
+Console.WriteLine(value);
+bool b = true;
+char c = '!';
+double d = 5,55;
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
